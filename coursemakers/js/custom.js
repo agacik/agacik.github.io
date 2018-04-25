f2366db1ec298b8a6a883cef2ccff35692dccfad
Agata Biesaga <abiesaga@gmail.com>
2018-04-25 20:40:15 +0200

    Dodanie efektu wskaniwanania labeli poprzez dodanie funkcji w js i ostylowanie labeli dla text i email.

--- coursemakers/js/custom.js
+++ coursemakers/js/custom.js
@@ -9,3 +9,18 @@ $(function() {
         }
     });
 });
+
+$(function () {
+    var showClass = 'show';
+
+    $('input').on('checkval', function () {
+        var label = $(this).prev('label');
+        if(this.value !== '') {
+            label.addClass(showClass);
+        } else {
+            label.removeClass(showClass);
+        }
+    }).on('keyup', function () {
+        $(this).trigger('checkval');
+    });
+});
