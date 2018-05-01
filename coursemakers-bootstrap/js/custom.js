b0d488a4f810f29a04b1f3866676044ccb8d8818
Agata Biesaga <abiesaga@gmail.com>
2018-05-01 04:08:54 +0200

    Zrobienie strony głównej od nowa na Bootstrap4 + mdbootstrap.com. Dodanie RWD oraz menu rozwijanego.

---
+++ coursemakers-bootstrap/js/custom.js
@@ -0,0 +1,21 @@
+$(function() {
+
+$("a").removeClass("waves-effect waves-light");
+
+$(function () {
+    'use strict';
+    var showClass = 'show';
+
+    $('input').on('checkval', function () {
+        var label = $(this).prev('label');
+        if(this.value !== '') {
+            label.addClass(showClass);
+        } else {
+            label.removeClass(showClass);
+        }
+    }).on('keyup', function () {
+        $(this).trigger('checkval');
+    });
+});
+    
+});
