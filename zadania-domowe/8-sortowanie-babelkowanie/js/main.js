55f289e9d5c4622624a900b5fce4a3a0abd85587
Agata Biesaga <abiesaga@gmail.com>
2018-05-02 11:56:38 +0200

    Zadanie domowe "8-sortowanie-babelkowanie".

--- zadania-domowe/8-sortowanie-babelkowanie/js/main.js
+++ zadania-domowe/8-sortowanie-babelkowanie/js/main.js
@@ -1,24 +1,24 @@
 'use strict';
 var tablica = [12, 67, 34, 23.01, 24, 2, 56, 8, 0x10, 23.02, 78, 34, 1e2, 45, 67, 98, 1];
 
-//function sortowanieBabelkowe(tablica)
-//{
-//    var wynik;
-//    do {
-//        wynik = false;
-//        for (var i=0; i < tablica.length - 1; i++) {
-//            if (tablica[i] > tablica[i+1]) {
-//                var zmienna = tablica[i];
-//                tablica[i] = tablica[i+1];
-//                tablica[i+1] = zmienna;
-//                wynik = true;
-//            }
-//        }
-//    } while (wynik);
-//}
-//
-//sortowanieBabelkowe(tablica);
-//console.log((tablica));
+function sortowanieBabelkowe(tablica)
+{
+    var wynik;
+    do {
+        wynik = false;
+        for (var i=0; i < tablica.length - 1; i++) {
+            if (tablica[i] > tablica[i+1]) {
+                var zmienna = tablica[i];
+                tablica[i] = tablica[i+1];
+                tablica[i+1] = zmienna;
+                wynik = true;
+            }
+        }
+    } while (wynik);
+}
+
+sortowanieBabelkowe(tablica);
+console.log((tablica));
 
 
 for (var i = 0; i < tablica.length; i++) {
