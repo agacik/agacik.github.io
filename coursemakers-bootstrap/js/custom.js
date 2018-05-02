822551a34238e3e3e279300fba1380aa427365a9
Agata Biesaga <abiesaga@gmail.com>
2018-05-02 21:23:11 +0200

    Dodanie efektu hover na dropdownmenu + poprawienie marginów w sekcji support.

--- coursemakers-bootstrap/js/custom.js
+++ coursemakers-bootstrap/js/custom.js
@@ -18,10 +18,16 @@ $(function () {
     });
 });
     
+    $('.nav-link').hover(
+        function(){ $(this).next('div.dropdown-menu-custom').addClass('show'), 
+                    $(this).parent().addClass('show')},
+        function(){ $(this).next('div.dropdown-menu-custom').removeClass('show'),
+                    $(this).parent().removeClass('show')});
+    $('div.dropdown-menu-custom').hover(
+        function(){ $(this).addClass('show')},
+        function(){ $(this).removeClass('show')});
+    
+    
+    
 });
 
-$(document).ready(function(){
-    $('.navbar-toggler-icon').click(function(){
-        $(this).toggleClass('open');
-    });
-}); 
