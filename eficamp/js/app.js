58db17d430c37e51be2027aba03a0aa8a6dc94d8
agacik <abiesaga@gmail.com>
2018-08-15 19:32:21 +0200

    Spotkanie 3 - EfiCamp.

--- eficamp/js/app.js
+++ eficamp/js/app.js
@@ -2,11 +2,18 @@
     console.log('Witamy na Campie 2018! ');
     
     const button = document.querySelector('#go');
+    const other = document.querySelector('#other');    
     const input = document.querySelector('#input');
     const result = document.querySelector('#result');
+    const close = document.querySelector('#close')
     
     button.addEventListener('click', () => {
         console.log('Click', input.value);
         result.innerText = input.value;
+        input.value = "";
+        input.placeholder = 'Hasło';
+        close.style.display = "inline-block";
+        other.style.visibility = "visible";
     })
+     
 })();
