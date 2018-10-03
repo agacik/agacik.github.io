f948dcdb0635e85e0e7cc1d51d6fae87a92bda22
agacik <abiesaga@gmail.com>
2018-10-03 13:57:00 +0200

    Zmiana na Bootstrapa 4.1.3, dodanie walidacji kolejnych etapów formuarza, dodanie dwóch pól w Dane nieruchomości (rodzaj, sprzedaż/wynajem).

--- nieruchomosci/formularz/js/main.js
+++ nieruchomosci/formularz/js/main.js
@@ -28,7 +28,7 @@ $(document).ready(function () {
         var curStep = $(this).closest(".setup-content"),
             curStepBtn = curStep.attr("id"),
             nextStepWizard = $('div.setup-panel div a[href="#' + curStepBtn + '"]').parent().next().children("a"),
-            curInputs = curStep.find("input[type='text'],input[type='url'],input[type='checkbox']"),
+            curInputs = curStep.find("input[type='text'],input[type='url'],input[type='checkbox'],input[type='radio']"),
             isValid = true;
 
         $(".form-group").removeClass("has-error");
@@ -40,12 +40,10 @@ $(document).ready(function () {
         }
 
         if (isValid)
-            nextStepWizard.removeAttr('disabled').trigger('click');
+            nextStepWizard.removeAttr('disabled').removeClass('disabled').trigger('click');
     });
 
     $('div.setup-panel div a.btn-primary').trigger('click');
-
-
 
 
 
@@ -59,26 +57,29 @@ $(document).ready(function () {
 
 
 
-
-
     navListItems.click(function (e) {
         e.preventDefault();
-        $(this).parent().removeClass('btn-check');
+        $(this).parent().next().removeClass('btn-check');
     });
 
 
-    $('.checkbox-show').click(function () {
-        $(this).toggleClass('checkbox-show').next().toggleClass('checkbox-active').show();
-        $(this).children().toggleClass('.fa-arrow-down').hide();
+    $('.read-more').click(function () {
+        $(this).hide();
+        $(this).next().show();
+        $(this).next().next().show();
     });
 
 
 
-    $('.checkbox-hidden').click(function () {
-        $(this).toggleClass('checkbox-active').hide();
-        $(this).prev().children().toggleClass('.fa-arrow-down').show();
+    $('.read-less').click(function () {
+        $(this).hide();
+        $(this).prev().show();
+        $(this).next().hide();
+    });
+
+
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
     })
-
-
 
 });
